4275a6b7eda76c288cba3a66935774bde32c7390
abusizhishen <abusizhishen@gmail.com>
2020-12-11 23:29:26 +0800

    log

--- src/http.go
+++ src/http.go
@@ -2,7 +2,6 @@ package src
 
 import (
 	"bytes"
-	"encoding/binary"
 	"fmt"
 	log "github.com/sirupsen/logrus"
 	"io"
@@ -16,7 +15,9 @@ type HttpProxy struct {
 }
 
 func (p *HttpProxy)Run(fun func(clientConn *net.TCPConn),port int) {
-	addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("0.0.0.0:%d", port))
+	addrStr := fmt.Sprintf("0.0.0.0:%d", port)
+	log.Infof("listen:%v", addrStr)
+	addr, err := net.ResolveTCPAddr("tcp", addrStr)
 	listen, err := net.ListenTCP("tcp", addr)
 	if err != nil {
 		panic(err)
@@ -47,108 +48,9 @@ func NewHttpClient(addr Addr)  {
 	proxy.Run(proxy.clientHandle, addr.LocalPort)
 }
 
-func HandlerSocks(clientConn net.TCPConn) {
-	defer clientConn.Close()
-	buf := make([]byte, 256)
-
-	/**
-	   The conn connects to the dstServer, and sends a ver
-	   identifier/method selection message:
-		          +----+----------+----------+
-		          |VER | NMETHODS | METHODS  |
-		          +----+----------+----------+
-		          | 1  |    1     | 1 to 255 |
-		          +----+----------+----------+
-	   The VER field is set to X'05' for this ver of the protocol.  The
-	   NMETHODS field contains the number of method identifier octets that
-	   appear in the METHODS field.
-	*/
-	// 第一个字段VER代表Socks的版本，Socks5默认为0x05，其固定长度为1个字节
-	_, err := clientConn.Read(buf)
-	// 只支持版本5
-	if err != nil || buf[0] != 0x05 {
-		log.Printf(" err %v,buf[0] :%d", err, buf[0])
-		return
-	}
-
-	/**
-	   The dstServer selects from one of the methods given in METHODS, and
-	   sends a METHOD selection message:
-
-		          +----+--------+
-		          |VER | METHOD |
-		          +----+--------+
-		          | 1  |   1    |
-		          +----+--------+
-	*/
-	// 不需要验证，直接验证通过
-	n, err := clientConn.Write([]byte{0x05, 0x00})
-	log.Printf("len,%d,err:%v", n, err)
-
-	/**
-	  +----+-----+-------+------+----------+----------+
-	  |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
-	  +----+-----+-------+------+----------+----------+
-	  | 1  |  1  | X'00' |  1   | Variable |    2     |
-	  +----+-----+-------+------+----------+----------+
-	*/
-
-	// 获取真正的远程服务的地址
-	n, err = clientConn.Read(buf)
-	// n 最短的长度为7 情况为 ATYP=3 DST.ADDR占用1字节 值为0x0
-	if err != nil || n < 7 {
-		log.Println("eeee")
-		return
-	}
-
-	// CMD代表客户端请求的类型，值长度也是1个字节，有三种类型
-	// CONNECT X'01'
-	if buf[1] != 0x01 {
-		// 目前只支持 CONNECT
-		return
-	}
-
-	var dIP []byte
-	// aType 代表请求的远程服务器地址类型，值长度1个字节，有三种类型
-	switch buf[3] {
-	case 0x01:
-		//	IP V4 address: X'01'
-		dIP = buf[4 : 4+net.IPv4len]
-	case 0x03:
-		//	DOMAINNAME: X'03'
-		ipAddr, err := net.ResolveIPAddr("ip", string(buf[5:n-2]))
-		if err != nil {
-			return
-		}
-		dIP = ipAddr.IP
-	case 0x04:
-		//	IP V6 address: X'04'
-		dIP = buf[4 : 4+net.IPv6len]
-	default:
-		log.Printf("unknown buf[3]:%v", buf[3])
-		return
-	}
-	dPort := buf[n-2:]
-	dstAddr := &net.TCPAddr{
-		IP:   dIP,
-		Port: int(binary.BigEndian.Uint16(dPort)),
-	}
-
-	log.Println(dstAddr)
-	buf = make([]byte, 1024)
-	for {
-		var n, err = clientConn.Read(buf)
-		if err != nil {
-			return
-		}
-
-		log.Printf("read content\n%s", buf[:n])
-	}
-}
-
 func (p *HttpProxy)serverHandle(client *net.TCPConn) {
 	defer client.Close()
-	log.Printf("remote addr: %v\n", client.RemoteAddr())
+	log.Infof("remote addr: %v\n", client.RemoteAddr())
 
 	// 用来存放客户端数据的缓冲区
 	var b [1024]byte
